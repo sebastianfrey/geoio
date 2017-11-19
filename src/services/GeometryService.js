e60fa8f5954079d108eea9dc1eeb6d9cdacf460c
sebastianfrey <sebastian.frey@outlook.com>
2017-11-19 19:58:30 +0100

    change default services url

--- src/services/GeometryService.js
+++ src/services/GeometryService.js
@@ -1,7 +1,7 @@
 import * as axios from 'axios';
 
-// axios.defaults.baseURL = "http://geoapplications.de/geoio/services/";
-axios.defaults.baseURL = "http://localhost:5000/";
+axios.defaults.baseURL = "http://geoapplications.de/geoio/services/";
+// axios.defaults.baseURL = "http://localhost:5000/";
 axios.defaults.headers.post['Content-Type'] = 'application/x-www-form-urlencoded';
 
 export function convexHull(collection) {
