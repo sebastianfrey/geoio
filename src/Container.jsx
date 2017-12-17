2f92fc151318794abdafbe86bf25518beaa18528
sebastianfrey <sebastian.frey@outlook.com>
2017-12-17 22:46:53 +0100

    add loading overlay

--- src/Container.jsx
+++ src/Container.jsx
@@ -15,32 +15,83 @@ import Notifications from 'react-notification-system-redux';
 
 const DropableMapController = Dropable(MapController);
 
+let loadingMessages = [
+  'Calibrating gravity',
+  'Loading atmosphere',
+  'Fastening seat belts',
+  'Ready, Set, Go!'
+];
+
+let timeout = 200;
+
 export class Container extends React.Component {
 
+  constructor(props) {
+    super(props);
+    this.state = {
+      message: "",
+      loading: true
+    };
+  }
+
+  componentDidMount() {
+    this.displayLoadingMessages(); 
+  }
+
+  displayLoadingMessages() {
+    let message = loadingMessages.shift();
+    
+    this.setState({
+      message: message
+    });    
+
+    setTimeout(() => {
+      if (loadingMessages.length > 0) {
+        this.displayLoadingMessages();
+      } else {
+        this.setState({ loading: false });
+      }
+    }, timeout);
+    timeout += 100;
+  }
+
   render() {
-    const {notifications} = this.props;
+    const { notifications } = this.props;
+    const { message, loading } = this.state;
 
     const style = {
       NotificationItem: {
-        error: { 
+        error: {
           color: 'red'
         }
       }
     };
-    
 
     return (
-      <div className="main">
-        <Header  title="GeoIO"/>
-        <div className="app-container">
-          <LayerController
-            title={"Table of Contents"}
-            right={true}/> 
-          <div className="content-container">
-            <DropableMapController />
-          </div>    
+      <div className={loading ? 'main loading' : 'main'}>
+        <div className="loading-overlay">
+          <div className="spinner">
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+          </div>
+          <img className="loading-image" src="images/logo-large.png" alt="" 
+            style={{width: '192px'}}/>
+          <div className="loading-message">{message}</div> 
         </div>
-        <Notifications notifications={notifications} style={style} />
+        <div className="container">
+          <Header  title="GeoIO"/>
+          <div className="app-container">
+            <LayerController
+              title={"Table of Contents"}
+              right={true}/> 
+            <div className="content-container">
+              <DropableMapController />
+            </div>    
+          </div>
+          <Notifications notifications={notifications} style={style} />
+        </div>
       </div>
     );
   }
--- src/Header.jsx
+++ src/Header.jsx
@@ -2,9 +2,13 @@ import React from 'react';
 
 export default class Header extends React.Component {
   render() {
+
+    const imgStyle = { width: '2.5rem', paddingLeft: '1.25rem'};
+
     return (
       <div className="header">
         <div className="title">{this.props.title}</div>
+        <img src="images/logo.png" alt="" style={imgStyle}/>
       </div>
     );
   }
